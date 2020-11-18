f95966b861a20cb311c27c0f8e228ee5632107d2
Kevin Chan <kevinchan128@msn.com>
2020-11-17 18:19:26 -0800

    Revert "Revert "Merge pull request #1 from CSC-648-SFSU/dev-branch""
    This reverts commit 2ba982d94fed61651269e1439bd1b4b2ad81b39d.

---
+++ frontend/src/HeaderPages/MyPatients.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Directory extends Component {
+  render() {
+    return (
+      <div>
+        <h2> This is our patient directory! </h2>
+      </div>
+    );
+  }
+}
+export default Directory;
