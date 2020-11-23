1f7795ab7c2997f694cd52cdd667af961b27e5e4
Spencer-Heltsley <70540146+Spencer-Heltsley@users.noreply.github.com>
2020-11-23 14:48:34 -0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,147 +1,85 @@
-/*
-import React from "react";
-import styled from "styled-components";
-import { useTable, useExpanded } from "react-table";
-import axios from "axios";
-import { useMemo, useState, useEffect } from "react";
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import style from './App.css';
 
-import makeData from "./components/makeData";
+import SAMPLE_LOGIN_PAGE from "./pages/SAMPLE_LOGIN_PAGE";
+import HomePage from "./pages/HomePage.js";
+import ContentPage from "./pages/ContentPage.js";
+import ProfilePage from "./pages/ProfilePage.js";
+import PingsPage from "./pages/PingsPage.js";
+import PatientsPage from "./pages/PatientsPage.js";
+import UploadPage from "./pages/UploadPage.js";
 
-import { Styles } from "./TEtable";
-import Table from "./TEtable";
+class App extends React.Component{
 
-function App() {
-  const [data, setData] = useState([]);
-  //const columns = useMemo(() => COLUMNS, []);
-
-  useEffect(() => {
-    axios
-      .get("/getCurrentPatients")
-      .then((res) => {
-        setData(res.data.data.currentPatients);
-        console.log(res.data.data.currentPatients);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  //only can used inside a function
-  const COLUMNS = React.useMemo(
-    () => [
-      {
-        // Make an expander cell
-        Header: () => null, // No header
-        id: "expander", // It needs an ID
-        Cell: ({ row }) => (
-          // Use Cell to render an expander for each row.
-          // We can use the getToggleRowExpandedProps prop-getter
-          // to build the expander.
-          <span {...row.getToggleRowExpandedProps()}>
-            {row.isExpanded ? "[Close]" : "-View-"}
-          </span>
-        ),
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Remedium',
+      headerLinks: [
+        {title: 'Home', path: '/home'},
+        {title: 'My Patients', path: '/mypatients'},
+        {title: 'Upload', path: '/upload'},
+        {title: 'Pings', path: '/pings'},
+        {title: 'My Content', path: '/content'},
+        {title: 'Profile', path: '/profile'},
+      ],
+      home: {
+        title: 'Home Page'
       },
-      {
-        Header: "ID",
-        accessor: "id",
+      patients: {
+        title: 'Patients Page'
       },
-      {
-        Header: "Name",
-        columns: [
-          {
-            Header: "First Name",
-            accessor: "firstName",
-          },
-          {
-            Header: "Last Name",
-            accessor: "lastName",
-          },
-        ],
+      upload: {
+        title: 'Uploads Page'
       },
-      {
-        Header: "Info",
-        columns: [
-          {
-            Header: "Age",
-            accessor: "age",
-          },
-          {
-            Header: "Bio",
-            accessor: "bio",
-          },
-          {
-            Header: "Email",
-            accessor: "email",
-          },
-          {
-            Header: "ptID",
-            accessor: "ptID",
-          },
-        ],
+      pings: {
+        title: 'Notification (Ping) Page'
       },
-    ],
-    []
-  );
+      content: {
+        title: 'Content Repository'
+      },
+      profile: {
+        title: 'Profile Page'
+      }
+      
+    }
+  }
 
-  // Create a function that will render our row sub components
-  const renderRowSubComponent = React.useCallback(
-    ({ row }) => (
-      <pre
-        style={{
-          fontSize: "20px",
-        }}
-      >
-        <code>{JSON.stringify({ values: row.values }, null, 2)}</code>
-      </pre>
-    ),
-    []
-  );
+  render(){
 
-  return (
-    <Styles>
-      <Table
-        columns={COLUMNS}
-        data={data}
-        // We added this as a prop for our table component
-        // Remember, this is not part of the React Table API,
-        // it's merely a rendering option we created for
-        // ourselves
-        renderRowSubComponent={renderRowSubComponent}
-      />
-    </Styles>
-  );
-}
-
-export default App;
-*/
-
-import React from "react";
-import "./App.css";
-import Header from "./Header";
-import Home from "./HeaderPages/Home";
-import MyPatients from "./HeaderPages/MyPatients";
-import UploadContent from "./HeaderPages/UploadContent";
-import Notifications from "./HeaderPages/Notifications";
-import LoginRegister from "./HeaderPages/LoginRegister";
-import Profile from "./HeaderPages/Profile";
-import { Switch, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/mypatients" component={MyPatients} />
-        <Route exact path="/uploadcontent" component={UploadContent} />
-        <Route exact path="/notifications" component={Notifications} />
-        <Route exact path="/loginregister" component={LoginRegister} />
-        <Route exact path="/profile" component={Profile} />
-        <Route path="/profile/:id" component={Profile} />
-      </Switch>
-    </div>
-  );
+    return(
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="App-header">
+            <Navbar.Brand>Remedium</Navbar.Brand>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="center">
+                <Link className="nav-link" to="/home">Home</Link>
+                <Link className="nav-link" to="/mypatients">Patients</Link>
+                <Link className="nav-link" to="/upload">Upload</Link>
+                <Link className="nav-link" to="/pings">Pings</Link>
+                <Link className="nav-link" to="/content">Content</Link>
+                <Link className="nav-link" to="/profile">Profile</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
+            <Route exact path="/" component = {SAMPLE_LOGIN_PAGE}/>
+            <Route exact path="/home:roomId" component={HomePage} />
+          </Switch>
+          <Route path="/mypatients" render={() => <PatientsPage title={this.state.patients.title}/> }/>
+          <Route path="/upload" render={() => <UploadPage title={this.state.upload.title}/> }/>
+          <Route path="/pings" render={() => <PingsPage title={this.state.pings.title}/> }/>
+          <Route path="/content" render={() => <ContentPage title={this.state.content.title}/> }/>
+          <Route path="/profile" render={() => <ProfilePage title={this.state.profile.title}/> }/>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
