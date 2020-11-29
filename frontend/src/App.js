892883be3d1b94b11a62953e3791ade0e7f9c273
Kevin Chan <kevinchan128@msn.com>
2020-11-29 11:35:34 -0800

    Uploading Collin's Recent Work
    He's hooked up somethings for backend purposes such as Patient Profile pages, My Patient's page, as well as Assigning content for patients.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import style from './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import style from "./App.css";
 
 import SAMPLE_LOGIN_PAGE from "./pages/SAMPLE_LOGIN_PAGE";
 import HomePage from "./pages/HomePage.js";
@@ -16,128 +16,232 @@ import UploadPage from "./pages/UploadPage.js";
 import LoginPage from "./pages/LoginPage.js";
 import RegisterPage from "./pages/RegisterPage.js";
 //Kevin's Pages
-import PatientProfilePage from './pages/PatientProfilePage.js';
-import AssignPage from './pages/AssignPage.js';
-import ReportPage from './pages/ReportPage.js';
-import SearchPage from './pages/SearchPage.js';
-import ContentItemPage from './pages/ContentItemPage.js';
-import ItemPage2 from './pages/ItemPage2.js';
-import ContactPage from './pages/ContactPage.js';
+import PatientProfilePage from "./pages/PatientProfilePage.js";
+import AssignPage from "./pages/AssignPage.js";
+import ReportPage from "./pages/ReportPage.js";
+import SearchPage from "./pages/SearchPage.js";
+import ContentItemPage from "./pages/ContentItemPage.js";
+import ItemPage2 from "./pages/ItemPage2.js";
+import ContactPage from "./pages/ContactPage.js";
 
-
-
-class App extends React.Component{
-
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      title: 'Remedium',
+      title: "Remedium",
       headerLinks: [
-        {title: 'Home', path: '/home'},
-        {title: 'My Patients', path: '/mypatients'},
-        {title: 'Upload', path: '/upload'},
-        {title: 'Pings', path: '/pings'},
-        {title: 'My Content', path: '/content'},
-        {title: 'Profile', path: '/profile'},
-        {title: 'login', path: '/login'},
-        {title: 'register', path: '/register'},
-        {title: 'patientProfile', path: '/patientProfile'},
-        {title: 'assign', path: '/assign'},
-        {title: 'report', path: '/progressreport'},
-        {title: 'contentitem', path: '/contentitem'},
-        {title: 'itempage2', path: '/itempage2'},
-        {title: 'contact', path: '/contact'}
+        { title: "Home", path: "/home" },
+        { title: "My Patients", path: "/mypatients" },
+        { title: "Upload", path: "/upload" },
+        { title: "Pings", path: "/pings" },
+        { title: "My Content", path: "/content" },
+        { title: "Profile", path: "/profile" },
+        { title: "login", path: "/login" },
+        { title: "register", path: "/register" },
+        { title: "patientProfile", path: "/patientProfile" },
+        { title: "assign", path: "/assign" },
+        { title: "report", path: "/progressreport" },
+        { title: "contentitem", path: "/contentitem" },
+        { title: "itempage2", path: "/itempage2" },
+        { title: "contact", path: "/contact" },
       ],
       home: {
-        title: 'Home Page'
+        title: "Home Page",
       },
       patients: {
-        title: 'Patients Page'
+        title: "Patients Page",
       },
       upload: {
-        title: 'Uploads Page'
+        title: "Uploads Page",
       },
       pings: {
-        title: 'Notification (Ping) Page'
+        title: "Notification (Ping) Page",
       },
       content: {
-        title: 'Content Repository'
+        title: "Content Repository",
       },
       profile: {
-        title: 'Profile Page'
+        title: "Profile Page",
       },
       login: {
-        title: 'Login Page'
+        title: "Login Page",
       },
       register: {
-        title: 'Register Page'
+        title: "Register Page",
       },
       patientProfile: {
-        title: 'Patient Profile Page'
+        title: "Patient Profile Page",
       },
       assign: {
-        title: 'Assign Content Page'
+        title: "Assign Content Page",
       },
       report: {
-        title: 'Progress Report Page'
+        title: "Progress Report Page",
       },
       search: {
-        title: 'Search Page'
+        title: "Search Page",
       },
       contentItem: {
-        title: 'Content Item Page'
+        title: "Content Item Page",
       },
       item2: {
-        title: 'Content Item Page'
+        title: "Content Item Page",
       },
       contact: {
-        title: 'Contact Page'
-      }
-      
-    }
+        title: "Contact Page",
+      },
+    };
   }
 
-  render(){
-
-    return(
+  render() {
+    return (
       <Router>
         <Container className="p-0" fluid={true}>
           <Navbar className="App-header">
             <Navbar.Brand>Remedium</Navbar.Brand>
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="center">
-                <Link className="nav-link" to="/home">Home</Link>
-                <Link className="nav-link" to="/mypatients">Patients</Link>
-                <Link className="nav-link" to="/upload">Upload</Link>
-                <Link className="nav-link" to="/pings">Pings</Link>
-                <Link className="nav-link" to="/content">Content</Link>
-                <Link className="nav-link" to="/profile">Profile</Link>
-                <Link className="nav-link" to="/login">Login</Link>
-                <Link className="nav-link" to="/register">Register</Link>
-                <Link className="nav-link" to="/patientprofile">Patient Profile</Link>
+                <Link className="nav-link" to="/home">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/mypatients">
+                  Patients
+                </Link>
+                <Link className="nav-link" to="/upload">
+                  Upload
+                </Link>
+                <Link className="nav-link" to="/pings">
+                  Pings
+                </Link>
+                <Link className="nav-link" to="/content">
+                  Content
+                </Link>
+                <Link className="nav-link" to="/profile">
+                  Profile
+                </Link>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+                <Link className="nav-link" to="/patientprofile">
+                  Patient Profile
+                </Link>
                 {/* <Link className="nav-link" to="/assign">Assign Content</Link> */}
-                <Link className="nav-link" to="/search">Search</Link>
+                <Link className="nav-link" to="/search">
+                  Search
+                </Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
           <Switch>
-            <Route exact path="/" component = {SAMPLE_LOGIN_PAGE}/>
+            <Route exact path="/" component={SAMPLE_LOGIN_PAGE} />
             <Route exact path="/home:roomId" component={HomePage} />
           </Switch>
-          <Route path="/mypatients" render={() => <PatientsPage title={this.state.patients.title}/> }/>
-          <Route path="/upload" render={() => <UploadPage title={this.state.upload.title}/> }/>
-          <Route path="/pings" render={() => <PingsPage title={this.state.pings.title}/> }/>
-          <Route path="/content" render={() => <ContentPage title={this.state.content.title}/> }/>
-          <Route path="/profile" render={() => <ProfilePage title={this.state.profile.title}/> }/>
-          <Route path='/login' render={() => <LoginPage title={this.state.login.title}/> }/>
-          <Route path='/register' render={() => <RegisterPage title={this.state.register.title}/> }/>
-          <Route path='/patientprofile' render={() => <PatientProfilePage title={this.state.patientProfile.title}/> }/>
-          <Route path='/assign' render={() => <AssignPage title={this.state.assign.title}/> }/>
-          <Route path='/progressreport' render={() => <ReportPage title={this.state.report.title}/> }/>
-          <Route path='/search' render={() => <SearchPage title={this.state.search.title}/> }/>
-          <Route path='/contentitem' render={() => <ContentItemPage title={this.state.contentItem.title}/> }/>
-          <Route path='/itempage2' render={() => <ItemPage2 title={this.state.item2.title}/> }/>
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title}/> }/>
+          <Route
+            exact
+            path="/mypatients"
+            render={() => <PatientsPage title={this.state.patients.title} />}
+          />
+          <Route
+            exact
+            path="/upload"
+            render={() => <UploadPage title={this.state.upload.title} />}
+          />
+          <Route
+            exact
+            path="/pings"
+            render={() => <PingsPage title={this.state.pings.title} />}
+          />
+          <Route
+            exact
+            path="/content"
+            render={() => <ContentPage title={this.state.content.title} />}
+          />
+          <Route
+            exact
+            path="/profile"
+            render={() => <ProfilePage title={this.state.profile.title} />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={() => <LoginPage title={this.state.login.title} />}
+          />
+          <Route
+            exact
+            path="/register"
+            render={() => <RegisterPage title={this.state.register.title} />}
+          />
+          <Route
+            exact
+            path="/patientprofile"
+            render={() => (
+              <PatientProfilePage title={this.state.patientProfile.title} />
+            )}
+          />
+          <Route
+            path="/patientprofile/:id"
+            render={() => (
+              <PatientProfilePage title={this.state.patientProfile.title} />
+            )}
+          />
+
+          <Route
+            exact
+            path="/assign"
+            render={() => <AssignPage title={this.state.assign.title} />}
+          />
+          <Route
+            path="/assign/:id"
+            render={() => <AssignPage title={this.state.assign.title} />}
+          />
+          <Route
+            exact
+            path="/progressreport"
+            render={() => <ReportPage title={this.state.report.title} />}
+          />
+          <Route
+            path="/progressreport/:id"
+            render={() => <ReportPage title={this.state.report.title} />}
+          />
+          <Route
+            exact
+            path="/search"
+            render={() => <SearchPage title={this.state.search.title} />}
+          />
+          <Route
+            exact
+            path="/contentitem"
+            render={() => (
+              <ContentItemPage title={this.state.contentItem.title} />
+            )}
+          />
+          <Route
+            path="/contentitem/:id"
+            render={() => (
+              <ContentItemPage title={this.state.contentItem.title} />
+            )}
+          />
+          <Route
+            exact
+            path="/itempage2"
+            render={() => <ItemPage2 title={this.state.item2.title} />}
+          />
+          <Route
+            path="/itempage2/:id"
+            render={() => <ItemPage2 title={this.state.item2.title} />}
+          />
+          <Route
+            exact
+            path="/contact"
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
+          <Route
+            path="/contact/:id"
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
         </Container>
       </Router>
     );
--- frontend/src/components/ProfileCard/ProfileCard.js
+++ frontend/src/components/ProfileCard/ProfileCard.js
@@ -1,22 +1,65 @@
-import React, {Component} from 'react';
-import '../ProfileCard/ProfileCard.css';
+import React, { Component } from "react";
+import CircularProgressWithLabel from "../MaterialUIcomponents/CircularProgressWithLabel";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Button, ButtonGroup } from "react-bootstrap";
 
-import { Card } from 'react-bootstrap';
+import "../ProfileCard/ProfileCard.css";
+
+import { Card } from "react-bootstrap";
 
 export class ProfileCard extends Component {
-    render() {
-        return(
-            <>
-            <Card className="ProfileCard" style={this.props.style}>
-                <h2>Full Name Here</h2>
-                <img id="profilepic" src="https://fertilitynetworkuk.org/wp-content/uploads/2017/01/Facebook-no-profile-picture-icon-620x389.jpg" alt="No display" />
-                <br></br>
-                {/* <p>Full Name</p> */}
-                <p>Location: SF, CA</p>
-                <p>Age: 15</p>
-                <p>Current Injury Type: </p>
-          </Card>
-          </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Card
+          className="ProfileCard"
+          style={{ width: "65%", marginLeft: "15%", height: "150%" }}
+        >
+          <h3 style={{ textAlign: "center" }}>
+            {this.props.first_name} {this.props.last_name}
+          </h3>
+          <img
+            id="profilepic"
+            src={this.props.avatar}
+            alt="No display"
+            style={{
+              width: "250px",
+              height: "180px",
+              borderRadius: "0%",
+              marginTop: "20px",
+              marginLeft: "25%",
+            }}
+          />
+          <br></br>
+          {/* <p>Full Name</p> */}
+          <p>Location: {this.props.location}</p>
+          <p>Age: {this.props.age}</p>
+          <p>Current Injury Type: {this.props.current_injury_type}</p>
+          <p>
+            Progress:{" "}
+            <CircularProgressWithLabel
+              variant="determinate"
+              value={this.props.progress}
+            />
+          </p>
+          <Link to={"/patientprofile/" + this.props.id}>
+            <Button
+              style={{
+                width: "80px", // whatever your button's width
+                margin: "0 auto", // auto left/right margins
+                display: "block",
+                /*
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                */
+              }}
+            >
+              Profile
+            </Button>
+          </Link>
+        </Card>
+      </>
+    );
+  }
 }
